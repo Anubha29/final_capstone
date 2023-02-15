0daa7c6c6f27bafed6b7a3be8849f3f917bce1ab
Anubha Mathur <anumathur29@gmail.com>
2023-02-15 21:28:03 +0530

    Update provder.tf
    upgrad-tfstate-file-capstone1 on S3

--- terraform/provder.tf
+++ terraform/provder.tf
@@ -10,7 +10,7 @@ terraform {
     }
   }
 backend "s3"{
-  bucket = "upgrad-tfstate-file-capstone"
+  bucket = "upgrad-tfstate-file-capstone1"
   key = "terraform.tfstate"
   region = "us-east-1"
 }
